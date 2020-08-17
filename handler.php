a575105b2e418a3d84e9300e332d1ef4015121bd
Mingleemma <mingleemma09@gmail.com>
2020-08-17 23:23:30 +0000

    Addend

--- handler.php
+++ handler.php
@@ -1,6 +1,6 @@
 <?php
     if (isset($_GET)) {
-        echo "<div style='padding:20%'><h1>" . $_GET['user_name']. "</h1>";
+        echo "<div style='color:blue; padding:20%'><h1>" . $_GET['user_name']. "</h1>";
         echo "<br><a href='mailto:".$_GET['user_mail']. "'>" . $_GET['user_mail']. "</a></div>";}
     else {
         echo "Abonkele";
